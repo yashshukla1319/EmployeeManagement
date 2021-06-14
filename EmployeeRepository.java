7942c04aab84365a1f65d9fa560554af0c20d6d7
yashshukla1319 <81147963+yashshukla1319@users.noreply.github.com>
2021-06-14 10:56:25 +0530

    Delete EmployeeRepository.java

--- EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ifour.EmployeeManagement.Employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
-}
